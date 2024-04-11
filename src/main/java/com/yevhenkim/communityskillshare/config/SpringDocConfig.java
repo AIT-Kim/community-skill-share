b62cdda53a14112136e956d00a0a23d12404a855
Yevhen Kim <evgeniikim@gmail.com>
2024-04-11 03:03:21 +0200

    *

---
+++ src/main/java/com/yevhenkim/communityskillshare/config/SpringDocConfig.java
@@ -0,0 +1,30 @@
+package com.yevhenkim.communityskillshare.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Community Skill Share API")
+                        .version("1.0")
+                        .description("API for Community Skill Share Application")
+                        .contact(new Contact().name("Yevhen Kim").email("evgeniikim@gmail.com")));
+    }
+}
